93827beab6a4f31393673a70655d930bb9b207fb
yinonhever <yinonehever@gmail.com>
2020-07-24 13:57:33 +0300

    adding zoom hover effect

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,15 +4,18 @@ import DetailLine from "./DetailLine";
 
 const Card = props => (
     <Fade bottom duration={700}>
-        <div className="card">
-            <img className="card__flag" src={props.flag} alt={props.name} />
-            <div className="card__content">
-                <h3 className="card__title">{props.name}</h3>
-                <DetailLine number title="Population" value={props.population} />
-                <DetailLine title="Region" value={props.region} />
-                <DetailLine title="Capital" value={props.capital} />
+        <div style={{height: "100%"}}>
+            <div className="card">
+                <img className="card__flag" src={props.flag} alt={props.name} />
+                <div className="card__content">
+                    <h3 className="card__title">{props.name}</h3>
+                    <DetailLine number title="Population" value={props.population} />
+                    <DetailLine title="Region" value={props.region} />
+                    <DetailLine title="Capital" value={props.capital} />
+                </div>
             </div>
         </div>
+
     </Fade>
 )
 
