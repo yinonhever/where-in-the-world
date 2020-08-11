d87b286e09017214a34d7752ffb978534e0fff97
yinonhever <yinonehever@gmail.com>
2020-08-11 13:41:34 +0300

    another little improvement

--- src/components/DetailContent.js
+++ src/components/DetailContent.js
@@ -5,6 +5,8 @@ import DetailLine from "./DetailLine";
 import Button from "./Button";
 
 const DetailContent = props => {
+    const { details, borders } = props;
+
     const mapWithCommas = (array, prop) => {
         const checkForEmptyValue = index => {
             if (prop) {
@@ -33,38 +35,38 @@ const DetailContent = props => {
     return (
         <Zoom duration={600}>
             <div className="detail-page__container">
-                <img className="detail-page__flag" src={props.details.flag} alt={props.details.name} />
+                <img className="detail-page__flag" src={details.flag} alt={details.name} />
                 <div className="detail-page__content">
-                    <h2 className="detail-page__heading">{props.details.name}</h2>
+                    <h2 className="detail-page__heading">{details.name}</h2>
                     <div className="detail-page__details">
                         <div className="detail-page__column">
-                            <DetailLine title="Native Name" value={props.details.nativeName} />
-                            <DetailLine number title="Population" value={props.details.population} />
-                            <DetailLine title="Region" value={props.details.region} />
-                            <DetailLine title="Sub Region" value={props.details.subregion} />
-                            <DetailLine title="Capital" value={props.details.capital} />
+                            <DetailLine title="Native Name" value={details.nativeName} />
+                            <DetailLine number title="Population" value={details.population} />
+                            <DetailLine title="Region" value={details.region} />
+                            <DetailLine title="Sub Region" value={details.subregion} />
+                            <DetailLine title="Capital" value={details.capital} />
                         </div>
                         <div className="detail-page__column">
                             <DetailLine
                                 title="Top Level Domain"
-                                value={mapWithCommas(props.details.topLevelDomain)}
+                                value={mapWithCommas(details.topLevelDomain)}
                             />
                             <DetailLine
                                 title="Currencies"
-                                value={mapWithCommas(props.details.currencies, "name")}
+                                value={mapWithCommas(details.currencies, "name")}
                             />
                             <DetailLine
                                 title="Languages"
-                                value={mapWithCommas(props.details.languages, "name")}
+                                value={mapWithCommas(details.languages, "name")}
                             />
                         </div>
                     </div>
                     <div className="detail-page__buttons-row" style={{
-                        display: props.borders.length === 0 ? "none" : "flex"
+                        display: borders.length === 0 ? "none" : "flex"
                     }}>
                         <span className="detail-page__buttons-title">Border Countries:</span>
                         <div className="detail-page__buttons">
-                            {props.borders.map(border => (
+                            {borders.map(border => (
                                 <Link
                                     to={"/details/" + border.code}
                                     key={border.code}
